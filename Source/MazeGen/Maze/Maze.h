4d879b101875a4ea3437092060955c7c64e4138d
Denis Maximenko <manlok71@gmail.com>
2021-10-25 17:37:13 +0300

    Maze WIP

--- Source/MazeGen/Maze/Maze.h
+++ Source/MazeGen/Maze/Maze.h
@@ -6,7 +6,19 @@
 #include "GameFramework/Actor.h"
 #include "Maze.generated.h"
 
-UCLASS()
+class ACell;
+class AWall;
+
+USTRUCT()
+class FArray2D
+{
+	GENERATED_BODY()
+
+public:
+
+};
+
+UCLASS(BlueprintType, Blueprintable)
 class MAZEGEN_API AMaze : public AActor
 {
 	GENERATED_BODY()
@@ -16,6 +28,37 @@ public:
 
 	virtual void Tick(float DeltaTime) override;
 
+	int GetLength() const;
+	UFUNCTION(BlueprintCallable)
+	void SetLength(int NewLength);
+
+	int GetWidth() const;
+	UFUNCTION(BlueprintCallable)
+	void SetWidth(int NewWidth);
+
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void AddCell(int X, int Y);
+	UFUNCTION(BlueprintNativeEvent)
+	void HandleCellAdded(ACell* NewCell);
+
+	UFUNCTION()
+	void AddWall(int X, int Y);
+	UFUNCTION(BlueprintNativeEvent)
+	void HandleWallAdded(AWall* NewWall);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	int Length;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	int Width;
+
+	UPROPERTY()
+	TArray<AWall*> WallsNS;
+	UPROPERTY()
+	TArray<AWall*> WallsWE;
+	UPROPERTY()
+	TArray<ACell*> Cells;
 };
