da06198df34ea2cb851e8f13c7f966ed3cc8673b
Denis Maximenko <manlok71@gmail.com>
2021-10-25 17:38:17 +0300

    Add function library for utility functions (A* etc.)

---
+++ Source/MazeGen/Maze/MazeUtils.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MazeUtils.generated.h"
+
+class AMaze;
+class ACell;
+class AWall;
+
+UCLASS()
+class MAZEGEN_API UMazeUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+};
